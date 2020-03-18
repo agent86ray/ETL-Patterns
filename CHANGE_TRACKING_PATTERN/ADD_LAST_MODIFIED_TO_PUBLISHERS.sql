16dc91e31378c281153b141e1d9bb030c2f285e7
Raymond Barley <raymondbarley@itresourcepartners.com>
2020-03-18 05:41:07 -0400

    Update [LAST_MODIFIED]

--- CHANGE_TRACKING_PATTERN/ADD_LAST_MODIFIED_TO_PUBLISHERS.sql
+++ CHANGE_TRACKING_PATTERN/ADD_LAST_MODIFIED_TO_PUBLISHERS.sql
@@ -9,8 +9,13 @@ ADD [LAST_MODIFIED] DATE NOT NULL
 DEFAULT GETDATE();
 GO
 
+-- set [LAST_MODIFIED] to a couple of days ago
+-- so we can see changes when we test
+UPDATE [pubs_snapshot].[dbo].[publishers]
+SET [LAST_MODIFIED] = '20200301';
+GO
+
 CREATE NONCLUSTERED INDEX NC_publishers_LAST_MODIFIED
 ON [dbo].[publishers] ([LAST_MODIFIED])
 GO
-
 
