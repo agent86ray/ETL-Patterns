c9e43319c07ba115b5a9b42eff0d3041011b8514
Raymond Barley <raymondbarley@itresourcepartners.com>
2020-03-18 05:42:50 -0400

    Add drop snapshot if exists.

--- CHANGE_TRACKING_PATTERN/CREATE_DATABASE_SNAPSHOT.sql
+++ CHANGE_TRACKING_PATTERN/CREATE_DATABASE_SNAPSHOT.sql
@@ -3,8 +3,23 @@
 
 	Change FILENAME path below as necessary
 */
-USE pubs
+USE master
 GO
+
+-- drop the snapshot if it exists
+IF EXISTS (
+	SELECT 
+		s1.[name]			AS [SNAPSHOT_DATABASE]
+	,	s1.[create_date]	AS [SNAPSHOT_CREATE_DATE]
+	,	s0.[name]			AS [SNAPSHOT_OF_DATABASE]
+	FROM sys.databases s1
+	JOIN sys.databases s0
+		ON s0.[database_id] = s1.[source_database_id]
+	WHERE s1.[name] = 'pubs_snapshot'
+)
+BEGIN
+	DROP DATABASE pubs_snapshot;
+END
 
 CREATE DATABASE pubs_snapshot
 ON  (
